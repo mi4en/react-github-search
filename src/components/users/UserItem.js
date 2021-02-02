bb6119450faac1ff164c7de410c5f2d61b03e6ee
Mario <marionikolov87@gmail.com>
2021-02-02 07:19:07 +0200

    add useritem component with state;

---
+++ src/components/users/UserItem.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class UserItem extends Component {
+	// We can use the constructor to have our state but it is a bit overkill if we dont need it for anything else
+	// constructor() {
+	// 	super();
+	// 	this.state = {
+	// 		id: 'id',
+	// 		login: 'mojombo',
+	// 		avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
+	// 		html_url: 'https://github.com/mojombo',
+	// 	};
+	// }
+
+	state = {
+		id: 'id',
+		login: 'mojombo',
+		avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
+		html_url: 'https://github.com/mojombo',
+	};
+
+	static propTypes = {};
+
+	render() {
+		const { login, avatar_url, html_url } = this.state;
+
+		return (
+			<div className='card text-center'>
+				<img
+					src={avatar_url}
+					alt=''
+					className='round-img'
+					style={{ width: '60px' }}
+				/>
+				<h3>{login}</h3>
+				<div>
+					<a
+						href={html_url}
+						target='_blanc'
+						className='btn btn-dark btn-sm my-1'
+					>
+						More
+					</a>
+				</div>
+			</div>
+		);
+	}
+}
+
+export default UserItem;
