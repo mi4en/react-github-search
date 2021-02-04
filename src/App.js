51a22b9be4b0296b37c1c231b18a32ff86de4e94
Mario <marionikolov87@gmail.com>
2021-02-04 07:59:46 +0200

    setup router; add search validation; create alert component; add search alert;

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import axios from 'axios';
 import './App.css';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
 import Search from './components/users/Search';
+import Alert from './components/layout/Alert';
 
 class App extends Component {
 	state = {
 		users: [],
 		loading: false,
+		alert: null,
 	};
 
 	// Search Github Users
@@ -30,21 +33,45 @@ class App extends Component {
 	// Clear Users from State
 	clearUsers = () => this.setState({ users: [], loading: false });
 
+	// Alert State
+	setAlert = (msg, type) => {
+		this.setState({ alert: { msg, type } });
+
+		setTimeout(() => {
+			this.setState({ alert: null });
+		}, 3000);
+	};
+
 	render() {
 		const { loading, users } = this.state;
 
 		return (
-			<div className='App'>
-				<Navbar />
-				<div className='container'>
-					<Search
-						searchUsers={this.searchUsers}
-						clearUsers={this.clearUsers}
-						showClear={users.length > 0 ? true : false}
-					/>
-					<Users loading={loading} users={users} />
+			<Router>
+				<div className='App'>
+					<Navbar />
+					<div className='container'>
+						<Alert alert={this.state.alert} />
+						<Switch>
+							<Route
+								exact
+								path='/'
+								render={(props) => (
+									<Fragment>
+										<Search
+											searchUsers={this.searchUsers}
+											clearUsers={this.clearUsers}
+											showClear={users.length > 0 ? true : false}
+											setAlert={this.setAlert}
+										/>
+									</Fragment>
+								)}
+							></Route>
+						</Switch>
+
+						<Users loading={loading} users={users} />
+					</div>
 				</div>
-			</div>
+			</Router>
 		);
 	}
 }
--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -10,14 +10,20 @@ export class Search extends Component {
 		searchUsers: PropTypes.func.isRequired,
 		clearUsers: PropTypes.func.isRequired,
 		showClear: PropTypes.bool.isRequired,
+		setAlert: PropTypes.func.isRequired,
 	};
 
 	onChange = (e) => this.setState({ [e.target.name]: e.target.value });
 
 	onSubmit = (e) => {
 		e.preventDefault();
-		this.props.searchUsers(this.state.text);
-		this.setState({ text: '' });
+
+		if (this.state.text === '') {
+			this.props.setAlert('Please enter search term', 'light');
+		} else {
+			this.props.searchUsers(this.state.text);
+			this.setState({ text: '' });
+		}
 	};
 
 	render() {
